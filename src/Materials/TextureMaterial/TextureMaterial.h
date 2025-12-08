9695bdfb7ffc079945d746b3a9e21ff6c433ff81
JuMassicotte <159472611+JuMassicotte@users.noreply.github.com>
2025-12-08 17:53:50 -0500

    3 fonctions

--- src/Materials/TextureMaterial/TextureMaterial.h
+++ src/Materials/TextureMaterial/TextureMaterial.h
@@ -18,7 +18,10 @@ public:
 
     virtual void displayInterface() {};
 
-
+    
+    void setDiffuseTexture(Texture2D *texture);
+    void setNormalMap(Texture2D *texture);
+    void setPhong(float kd, float ka, float ks, int s);
 
     void setCatDeform(bool d) { catDeform = d; }
 
