6d1ab979dcfef9d2e1780022d25e998365320aed
JuMassicotte <159472611+JuMassicotte@users.noreply.github.com>
2025-12-08 12:46:18 -0500

    distortion du matériel et ajouter à Kitty.

--- src/EngineGL.cpp
+++ src/EngineGL.cpp
@@ -5,6 +5,7 @@
 #include "Materials/BaseMaterial/BaseMaterial.h"
 #include "Materials/TextureMaterial/TextureMaterial.h"
 #include "Materials/MoveMaterial/MoveMaterial.h"
+#include "Materials/DistortionMaterial/DistortionMaterial.h"
 
 #include "Texture2D.h"
 
@@ -64,7 +65,7 @@ bool EngineGL::init() {
 
     Texture2D *textureKitty = new Texture2D(ObjPath + "Textures/Cat/concrete_cat_statue_diff_4k.jpg");
     Texture2D *textureKittyN = new Texture2D(ObjPath + "Textures/Cat/concrete_cat_statue_nor_gl_4k.jpg");
-    TextureMaterial *matKitty = new TextureMaterial("matKitty");
+    TextureMaterial *matKitty = new DistortionMaterial("matKitty");
     matKitty->setDiffuseTexture(textureKitty);
     matKitty->setNormalMap(textureKittyN);
     matKitty->setPhong(glm::vec3(1), glm::vec3(0.5), glm::vec3(.1), 20.0);
